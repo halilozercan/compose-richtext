42ec2a37806502e17c7c6e2928a5623137ecdcb7
Zach Klippenstein <zach.klippenstein@gmail.com>
2021-05-18 22:13:18 -0700

    Reproduce RippleDrawable crash.

--- sample/src/main/java/com/zachklipp/richtext/sample/SampleActivity.kt
+++ sample/src/main/java/com/zachklipp/richtext/sample/SampleActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.ripple.ExperimentalRippleApi
-import androidx.compose.material.ripple.LocalRippleNativeRendering
-import androidx.compose.runtime.CompositionLocalProvider
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +11,7 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      // There's a bug with the new RippleDrawable implementation introduced in beta07 that causes
-      // a crash in some of the samples. See https://issuetracker.google.com/issues/188569367.
-      CompositionLocalProvider(LocalRippleNativeRendering provides false) {
-        SampleLauncher()
-      }
+      SampleLauncher()
     }
   }
 }
